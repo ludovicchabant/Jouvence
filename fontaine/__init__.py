d519a5230c7d6273e0074ac95adb1db2bddb36dd
Ludovic Chabant <ludovic@chabant.com>
2017-01-04 08:51:18 -0800

    Add PyPI support.

---
+++ fontaine/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from .version import version
+except ImportError:
+    version = '<unknown>'
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as fp:
+        return fp.read()
+
+
+def readlines(fname):
+    return [l.strip() for l in read(fname).strip().splitlines()]
+
+
+install_requires = readlines('requirements.txt')
+tests_require = readlines('dev-requirements.txt')
+long_description = read('README.rst')
+
+
+setup(
+    name="Fontaine",
+    use_scm_version={'write_to': 'fontaine/version.py'},
+    description="A library for parsing and rendering Fountain screenplays.",
+    long_description=long_description,
+    author="Ludovic Chabant",
+    author_email="ludovic@chabant.com",
+    license="Apache License 2.0",
+    url="https://bolt80.com/fontaine",
+    keywords='fountain screenplay screenwriting screenwriter',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=['setuptools_scm', 'pytest-runner'],
+    tests_require=tests_require,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'fontaine = fontaine.cli.main'
+        ]}
+)
